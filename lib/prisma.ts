ba7d386a452575c5d8ae005832de3e847cefccbf
rizkybp31 <rizkybayuprasetyo@gmail.com>
2026-01-30 02:58:18 +0700

    test without sll

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,35 +2,23 @@ import "dotenv/config";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import { PrismaClient } from "@/generated/prisma/client";
 
-const sslCert = process.env.DATABASE_CA_CERT?.replace(/\\n/g, "\n");
-
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
+if (!globalForPrisma.prisma) {
+  globalForPrisma.prisma = new PrismaClient({
     adapter: new PrismaMariaDb({
-      host: process.env.DATABASE_HOST,
-      user: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      port: Number(process.env.DATABASE_PORT),
-      connectionLimit: 10,
-      ssl: sslCert
-        ? {
-            ca: sslCert,
-            rejectUnauthorized: true,
-          }
-        : undefined,
+      host: process.env.DATABASE_HOST!,
+      user: process.env.DATABASE_USER!,
+      password: process.env.DATABASE_PASSWORD!,
+      database: process.env.DATABASE_NAME!,
+      port: Number(process.env.DATABASE_PORT) || 3306,
+      connectionLimit: 1, // 🔥 serverless-safe
+      ssl: false, // testing OK
     }),
-    log:
-      process.env.NODE_ENV === "development"
-        ? ["query", "error", "warn"]
-        : ["error"],
+    log: ["error"],
   });
-
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
 }
+
+export const prisma = globalForPrisma.prisma;
